c629894d89426499939e9c002e87ba929f0c6d92
marcosdanielvillalba10-create <marcosdanielvillalba10@gmail.com>
2026-06-23 20:07:05 -0300

    Add files via upload

---
+++ calculos.js
@@ -0,0 +1,54 @@
+const PRECIO_M2 = 22790;
+const VIATICO = 20000;
+
+const inputAncho = document.getElementById("anchoLona");
+const inputAlto = document.getElementById("altoLona");
+
+const metrosCuadrados = document.getElementById("metrosCuadrados");
+const precioEstimado = document.getElementById("precioEstimado");
+
+function calcular() {
+
+  const ancho = Number(inputAncho.value);
+  const alto = Number(inputAlto.value);
+
+  if (
+    ancho <= 0 ||
+    alto <= 0 ||
+    isNaN(ancho) ||
+    isNaN(alto)
+  ) {
+
+    metrosCuadrados.textContent =
+      "Metros cuadrados: —";
+
+    precioEstimado.textContent =
+      "Estimación: —";
+
+    return;
+  }
+
+  const m2 = ancho * alto;
+
+  const total =
+    (m2 * PRECIO_M2) +
+    VIATICO;
+
+  metrosCuadrados.textContent =
+    `Metros cuadrados: ${m2.toFixed(2)} m²`;
+
+  precioEstimado.textContent =
+    `Estimación: $${Math.round(total).toLocaleString("es-AR")}`;
+}
+
+inputAncho.addEventListener(
+  "input",
+  calcular
+);
+
+inputAlto.addEventListener(
+  "input",
+  calcular
+);
+
+calcular();
